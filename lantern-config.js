98b4d36317a911ccd695e4fa8177b4d798a66ded
peppasusu <2680071017@qq.com>
2026-05-09 19:38:47 +0800

    Add images, lantern-config, fonts

---
+++ lantern-config.js
@@ -0,0 +1,9 @@
+/**
+ * 扫码「保存成品图」：二维码里的图片地址 = 本前缀 + images/...
+ *
+ * 本仓库 Pages：https://peppasusu.github.io/peppasususudihieyw/
+ * （也可改为 ""，与当前浏览器地址同源时由页面自动拼路径。）
+ *
+ * 本地调试可暂时改为：http://电脑IPv4:5173/ ，并在本目录 npm start。
+ */
+window.LANTERN_PUBLIC_BASE_URL = "https://peppasusu.github.io/peppasususudihieyw/";
